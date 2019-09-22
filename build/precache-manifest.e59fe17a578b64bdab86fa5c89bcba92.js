b40099e4de22e30c088abc68729b59ec46141523
Ismail <mailismailpe@gmail.com>
2019-09-22 13:29:34 +0530

    removedbuildfromignr

---
+++ build/precache-manifest.e59fe17a578b64bdab86fa5c89bcba92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a344efea060476468db321cc7e029adc",
+    "url": "/tests/index.html"
+  },
+  {
+    "revision": "8af053e5a7f6c282b404",
+    "url": "/tests/static/css/main.53ca93dc.chunk.css"
+  },
+  {
+    "revision": "59ec5898e0391c3c24d7",
+    "url": "/tests/static/js/2.6c1fcc56.chunk.js"
+  },
+  {
+    "revision": "aa40789135ae4b5f4df2",
+    "url": "/tests/static/js/3.9ce6867a.chunk.js"
+  },
+  {
+    "revision": "728cb186995cd94e9577",
+    "url": "/tests/static/js/4.7fea7937.chunk.js"
+  },
+  {
+    "revision": "8af053e5a7f6c282b404",
+    "url": "/tests/static/js/main.499fb331.chunk.js"
+  },
+  {
+    "revision": "2cc2a13500905b6f49be",
+    "url": "/tests/static/js/runtime~main.c5b47989.js"
+  }
+]);
