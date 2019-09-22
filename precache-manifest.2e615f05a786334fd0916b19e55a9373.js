706105eb59a9d4c001f0a80d42b6e72ae5f8ad9b
Ismail <mailismailpe@gmail.com>
2019-09-22 15:18:59 +0530

    Updates

---
+++ precache-manifest.2e615f05a786334fd0916b19e55a9373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4024a81780bba83a20b3dd38219aaf56",
+    "url": "/tests/index.html"
+  },
+  {
+    "revision": "b3764adf6b5aba97ab41",
+    "url": "/tests/static/css/main.8ab7251b.chunk.css"
+  },
+  {
+    "revision": "59ec5898e0391c3c24d7",
+    "url": "/tests/static/js/2.6c1fcc56.chunk.js"
+  },
+  {
+    "revision": "2780b57848ff8eaa46aa",
+    "url": "/tests/static/js/3.dcf28a2b.chunk.js"
+  },
+  {
+    "revision": "728cb186995cd94e9577",
+    "url": "/tests/static/js/4.7fea7937.chunk.js"
+  },
+  {
+    "revision": "b3764adf6b5aba97ab41",
+    "url": "/tests/static/js/main.499fb331.chunk.js"
+  },
+  {
+    "revision": "66e0ec54e1d6bcc08c99",
+    "url": "/tests/static/js/runtime~main.bb64c179.js"
+  }
+]);
