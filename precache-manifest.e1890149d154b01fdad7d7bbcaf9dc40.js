755be43c4f815695b104eb57b6e11ed068d26ca9
Ismail <mailismailpe@gmail.com>
2019-10-22 19:20:11 +0530

    Updates

--- precache-manifest.e1890149d154b01fdad7d7bbcaf9dc40.js
+++ precache-manifest.e1890149d154b01fdad7d7bbcaf9dc40.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0c1be8daa0e9c80c6a0638362b79af52",
+    "revision": "8591bd93a2fd1f0fa77c34a2d2a4b61c",
     "url": "/tests/index.html"
   },
   {
-    "revision": "86c935b842b4ea7fc076",
+    "revision": "963e161d030edb7e4d11",
     "url": "/tests/static/css/main.b363faf3.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tests/static/js/4.df70e607.chunk.js"
   },
   {
-    "revision": "3ea5a98ac0c73ba7d9db",
-    "url": "/tests/static/js/5.fcb4bcb9.chunk.js"
+    "revision": "06d640c57f9d4f1a9edc",
+    "url": "/tests/static/js/5.9c4266aa.chunk.js"
   },
   {
     "revision": "5a3d10c1ceca0eb5b20e",
@@ -32,15 +32,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tests/static/js/7.2a28f9db.chunk.js"
   },
   {
-    "revision": "e98f8c96b6cf9c478e10",
-    "url": "/tests/static/js/8.7d730f01.chunk.js"
+    "revision": "e4bfa9c5deae18d9e587",
+    "url": "/tests/static/js/8.aebad7a9.chunk.js"
   },
   {
-    "revision": "86c935b842b4ea7fc076",
-    "url": "/tests/static/js/main.57ad24d8.chunk.js"
+    "revision": "963e161d030edb7e4d11",
+    "url": "/tests/static/js/main.32fdec0b.chunk.js"
   },
   {
-    "revision": "a1b2e6c785ed6d7f508f",
-    "url": "/tests/static/js/runtime~main.c5593c3e.js"
+    "revision": "b6eede457c3543e92b02",
+    "url": "/tests/static/js/runtime~main.ccb95f5f.js"
   }
 ]);
